1500ac38f1b8274cfaf6a9eb2013593159a4956c
Santiago Andrade <santiago.andrade@crifa.com>
2022-04-28 12:04:10 -0500

    Start Project

---
+++ src/main/java/com/example/SantiagoAndrade_inventarios/SantiagoAndradeInventariosApplication.java
@@ -0,0 +1,13 @@
+package com.example.SantiagoAndrade_inventarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantiagoAndradeInventariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantiagoAndradeInventariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SantiagoAndrade_inventarios/SantiagoAndradeInventariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SantiagoAndrade_inventarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantiagoAndradeInventariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
